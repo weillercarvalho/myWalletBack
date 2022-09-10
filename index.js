a6ba467696b72b4178dfe1268980f27c57ac3204
weillercarvalho <engweiller@hotmail.com>
2022-09-10 17:35:31 -0300

    FEAT [index.js] Minus/Plus

--- index.js
+++ index.js
@@ -20,20 +20,65 @@ let db;
 
 mongoClient.connect().then(() => {
     db = mongoClient.db(`mywallet`)
+});
+
+const singUpSchema = joi.object({
+    name: joi.string().empty(" ").min(1).max(30).required(),
+    email: joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'br'] } }).empty(" ").min(1).max(30).required(),
+    password: joi.string().empty(" ").min(6).max(30).pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
 })
 
-server.post(`/singin`, async (req,res) => {
-    const token = uuidv4(); 
-    console.log(req.body)
-    return res.send(token)
+const postValueSchema = joi.object({
+    value: joi.number().required(),
+    description:joi.string().empty(" ").required(),
+    type: joi.string().required()
 })
 
 server.post(`/singup`, async (req,res) => {
+    const {name, email, password} = req.body;
+    const validation = singUpSchema.validate(req.body, {abortEarly:false});
+    if (validation.error) {
+        const erroMessage = validation.error.details.map(value => value.message)
+        return res.status(422).send(console.log(erroMessage))
+    }
     try {
-        res.send(200)
+        const passwordHash = bcrypt.hashSync(password, 12);
+        await db.collection(`users`).insertOne({...req.body,password: passwordHash});
+        return res.send(201)
+
     } catch (error) {
         console.log(error)
+        return res.send(500)
     }
+})
+
+server.post(`/singin`, async (req,res) => {
+    const {email, password} = req.body;
+    try {
+        const user = await db.collection(`users`).findOne({email: email})
+        if (user && bcrypt.compareSync(password, user.password)) {
+            const token = uuidv4();
+            await db.collection(`sessions`).insertOne({userId: user._id, token})
+            return res.send({token: token, name: user.name});
+        }
+        else {
+            return res.status(401).send({message: `Email ou senha incorretos.`})
+        }
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+})
+
+server.post(`/addvalue`, async (req,res) => {
+    console.log(req.body);
+})
+
+server.post(`/removevalue`, async (req, res) => {
+    console.log(req.body);
+})
+
+server.get(`/result`, async (req,res) => {
+    
 })
 
 
